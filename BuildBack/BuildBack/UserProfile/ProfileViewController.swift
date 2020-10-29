1b50a157262e23bab9a3beb57c270d7c11717cac
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-10-29 15:35:38 -0400

    - Profile view controller now has a logout function/Button

--- BuildBack/BuildBack/UserProfile/ProfileViewController.swift
+++ BuildBack/BuildBack/UserProfile/ProfileViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
+    //MARK:- IBOutlets
+    
+    
+    
+    //MARK:- View Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemGroupedBackground
-        
+    }
+    //MARK:- IBActions
+    @IBAction func logoutButtonPressed(_ sender: UIBarButtonItem) {
+        DispatchQueue.main.async {
+            try? Auth.auth().signOut()
+        }
+        guard let loginController = UIStoryboard(name: "LoginViewRefactor", bundle: .main).instantiateInitialViewController(creator: { coder in
+                return IntroViewController(coder: coder)
+        }) else {
+            return
+        }
+        UIViewController.showviewController(viewController: loginController)
     }
     
-
    
 }
