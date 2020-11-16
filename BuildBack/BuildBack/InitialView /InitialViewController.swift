45969bae5948e16532b402424cd0f55a911815ff
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-11-16 13:01:42 -0500

    - Reordered some of the files in the hierarchy for easier browsing - Cleaned up and refactored a little bit of code in the "initial view controller"

--- BuildBack/BuildBack/InitialView /InitialViewController.swift
+++ BuildBack/BuildBack/InitialView /InitialViewController.swift
@@ -9,37 +9,34 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-        
-                let logoAnimationView = InitialView()
-                
-                override func viewDidLoad() {
-                    super.viewDidLoad()
-                    
-                    view.backgroundColor = .systemBackground
-                                                 //(deadline: .now() + 4.5)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 4.2) {
-                  
-                        
-                        let scene = UIApplication.shared.connectedScenes.first
-                        if let sceneDelegate = (scene?.delegate as? SceneDelegate) {
-                            UIView.transition(with: self.logoAnimationView,
-                                              duration: 3.0,
-                                                       options: .transitionCrossDissolve,
-                                                       animations: nil,
-                                                       completion: nil)
-                            sceneDelegate.afterSplashScreenAnimation()
-                        }
-                    }
-                 
-                }
-
-                
-                override func loadView() {
-                    view = logoAnimationView
-                }
-                
-                override func viewDidAppear(_ animated: Bool) {
-                    super.viewDidAppear(animated)
-                }
-                
+    
+    let logoAnimationView = InitialView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        loadGifAnimation()
+    }
+    override func loadView() {
+        view = logoAnimationView
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func loadGifAnimation() {
+        let animationTime = 4.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + animationTime) {
+            let scene = UIApplication.shared.connectedScenes.first
+            if let sceneDelegate = (scene?.delegate as? SceneDelegate) {
+                UIView.transition(with: self.logoAnimationView,
+                                  duration: 3.0,
+                                  options: .transitionCrossDissolve,
+                                  animations: nil,
+                                  completion: nil)
+                sceneDelegate.afterSplashScreenAnimation()
             }
+        }
+    }
+    
+}
